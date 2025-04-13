ff3225ca7ecf22ddcaf2e57a982140545d292341
Fiazkhan <147318675+Fiqhuar@users.noreply.github.com>
2025-04-13 21:39:30 +0530

    Update companyLogos.ts

--- companyLogos.ts
+++ companyLogos.ts
@@ -2,6 +2,7 @@ export const companyLogoMap: Record<string, string> = {
     amazon: "Amazon.png",
     microsoft: "Microsoft.png",
     swiggy: "Swiggy.png",
+    tesla: "Tesla.png",
     // Add more as needed
   };
   
